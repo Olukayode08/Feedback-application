1580e74de40aeb4fe28fc01a6621022a6ad789f8
Olukayode <olukayodeazeez2000@gmail.com>
2023-10-19 14:15:18 +0100

    Feedback completed

--- src/Context.js
+++ src/Context.js
@@ -1,34 +1,34 @@
-import React, { createContext, useState, useEffect } from 'react'
-import FeedbackData from './data/FeedbackData'
+import React, { createContext, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
 const Context = ({ children }) => {
   const [text, setText] = useState('')
-  const [isLoading, setIsLoading] = useState(true)
   const [rating, setRating] = useState(10)
-  const [feedback, setFeedback] = useState([])
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      rating: 10,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+  ])
   const [btnDisabled, setbtnDisabled] = useState(true)
   const [message, setMessage] = useState('')
   const [feedbackEdited, setFeedbackEdited] = useState({
     item: {},
     edit: false,
   })
-
-  useEffect(() => {
-    fetchFeedback()
-  }, [])
-
-  const fetchFeedback = async () => {
-    const response = await fetch(
-      `http://localhost:5000/feedback?_sort=id&_order=desc`
-    )
-    const data = await response.json()
-
-    setFeedback(data)
-    setIsLoading(false)
-  }
 
   // Turns button to disabled if text is less than 10 characters
   const handleTextChange = (e) => {
@@ -44,18 +44,36 @@ const Context = ({ children }) => {
     }
     setText(e.target.value)
   }
+  // When I click on send, it adds the feedback in the input to the feedback list
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
 
   // Delete feedback when I click on x
-  const deleteFeedback = async (id) => {
+  const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to Delete')) {
-      await fetch(`http://localhost:5000/feedback/${id}`, {
-        method: 'DELETE'
-      })
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
 
-    // When I click on send, it adds the feedback in the input to the feedback list
+  // On click Edit feedback
+  const editFeedback = (item) => {
+    setFeedbackEdited({
+      item,
+      edit: true,
+    })
+  }
+  // Update feedback text
+  const updateFeedback = (id, updatedItem) => {
+    setFeedback(
+      feedback.map((item) =>
+        item.id === id ? { ...item, ...updatedItem } : item
+      )
+    )
+  }
+
+  // When I click on send, it adds the feedback in the input to the feedback list
   const handleSubmit = (e) => {
     e.preventDefault()
     if (text.trim().length > 10) {
@@ -71,21 +89,6 @@ const Context = ({ children }) => {
       setText('')
     }
   }
-  // When I click on send, it adds the feedback in the input to the feedback list
-  const addFeedback = async (newFeedback) => {
-    // Adds the inputed text to the json data
-    const response = await fetch('http://localhost:5000/feedback', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      // To turn it into a json string
-      body: JSON.stringify(newFeedback)
-    })
-    const data = await response.json()
-    setFeedback([data, ...feedback])
-  }
-
 
   // Calculating average rating
   let average =
@@ -95,30 +98,6 @@ const Context = ({ children }) => {
 
   // Converting to 1 decimal place
   average = average.toFixed(1).replace(/[.,]0$/, '')
-
-  // On click Edit feedback
-  const editFeedback = (item) => {
-    setFeedbackEdited({
-      item,
-      edit: true,
-    })
-  }
-  // Update feedback text
-  const updateFeedback = async (id, updatedItem) => {
-    const response = await fetch(`http://localhost:5000/feedback/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(updatedItem),
-    })
-    const data = await response.json()
-    setFeedback(
-      feedback.map((item) =>
-        item.id === id ? { ...item, ...data } : item
-      )
-    )
-  }
 
   return (
     <>
@@ -140,7 +119,6 @@ const Context = ({ children }) => {
           editFeedback,
           feedbackEdited,
           updateFeedback,
-          isLoading,
         }}
       >
         {children}
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,26 +3,11 @@ import { motion, AnimatePresence } from 'framer-motion'
 import { FeedbackContext } from '../Context'
 import Card from './shared/Card'
 import { FaTimes, FaEdit } from 'react-icons/fa'
-import spinner from '../assets/spinner.png'
 
 const FeedbackList = () => {
-  const { feedback, deleteFeedback, editFeedback, isLoading } =
-    useContext(FeedbackContext)
-  if (!isLoading && (!feedback || feedback.length === 0)) {
-    return <p>No feedback yet</p>
-  }
+  const { feedback, deleteFeedback, editFeedback } = useContext(FeedbackContext)
 
-  return isLoading ? (
-    <img
-      src={spinner}
-      alt='Loading...'
-      style={{
-        width: '50%',
-        margin: 'auto',
-        display: 'block',
-      }}
-    />
-  ) : (
+  return (
     <div className='feedback-list'>
       <AnimatePresence>
         {feedback.map((item) => (
@@ -41,7 +26,6 @@ const FeedbackList = () => {
                 <FaEdit color='purple' />
               </button>
               <div className='text-display'>{item.text}</div>
-              {/* <button onClick={()=>console.log('123')}>Click</button> */}
             </Card>
           </motion.div>
         ))}
